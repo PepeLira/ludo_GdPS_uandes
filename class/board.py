cf4f7ac7cedc6e39f488e364a30720842566ba29
Valcalde <valcalde@miuandes.cl>
2023-09-15 14:13:29 -0300

    add class

---
+++ class/board.py
@@ -0,0 +1,6 @@
+import array
+
+
+class Board():
+    def __init__(self) -> None:
+        borad_1 = array.array('u', ['VACIO'] * 50)
---
+++ class/token.py
@@ -0,0 +1,7 @@
+
+class token():
+    def __init__(self,color = "W"):
+        self.color = color
+        self.color_token = color
+        self_number_token = 0
+        
